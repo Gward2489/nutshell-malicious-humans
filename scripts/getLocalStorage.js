781056188fde1b51959d85aef7cbc88f3e8862d9
Gward2489 <gww8804@gmail.com>
2017-11-16 14:23:52 -0600

    began nutshell clone

--- scripts/getLocalStorage.js
+++ scripts/getLocalStorage.js
@@ -1,7 +1,14 @@
 //Tyler: This module exports a function that pulls down local storage and parses it.
 
-let getLocalStorage = () => {
-    return JSON.parse(localStorage.getItem("nutshellDB"))
+let getLocalStorage = function () {
+    $.ajax({
+        "url": "database.json",
+        "method": "GET"
+    }).then(
+        function (nutshellDatabase) {
+            
+        }
+    )
 }
 
 module.exports = getLocalStorage
